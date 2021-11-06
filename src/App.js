7b6c38ba4efc01253c3a955aaa36e2839354b2b2
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-11-06 05:59:48 -0700

    add a header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
+import Header from "./components/Header"
 import PortfolioContainer from "./components/PortfolioContainer";
 
-const App = () => <PortfolioContainer />;
+const App = () => <div>
+    <Header />
+    <PortfolioContainer />;
+    </div>
 
 export default App;
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -19,11 +19,11 @@ function NavTabs({ currentPage, handlePageChange }) {
       <li className="nav-item">
         <a
           href="resume"
-          onClick={() => handlePageChange('resume')}
+          onClick={() => handlePageChange('Resume')}
           // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'resume' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
         >
-          resume
+          Resume
         </a>
       </li>
       <li className="nav-item">
