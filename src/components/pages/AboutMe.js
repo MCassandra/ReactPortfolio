4dc267816be28cef2e33f3797e9cb1cdd71b5534
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-11-08 12:17:01 -0800

    add photo headshot

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from 'react'
+import headshot from '../assets/'
 
 export default function AboutMe() {
   return (
     <div>
       <h1>Hello, There!</h1>
       <p>
-        add a picture here!
+      <img src={headshot}/>
       </p>
       <p>
-        I am a scientist transitioning my career into web developement. I have worked with animals for over 10 years. 
+        I am a scientist transitioning my career into web developement. I have worked with animals for over 10 years.
       </p>
       <p>I have been learning to code for the past 5 months with UC Irvine's Full Stack Flex Developement bootcamp. This journey has been wild! Web Developement has opened so many curiosities for me, and I can't wait to explore them. My dream would be using my coding skills to bridge my love for animals and science to this exciting world of web development.</p>
     </div>
